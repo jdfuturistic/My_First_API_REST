b7d1017c700ebd74f473bf7ff26cf6486f9a6ead
Joseph David <josephdavid200320@gmail.com>
2023-01-18 08:03:36 -0500

    Importación de paquete mongoose

--- servidor.js
+++ servidor.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+//DB Connection
+
+
+//CRUD operations
+
+
 app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto 3000")
 });
